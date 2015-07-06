3827fd7875a7408f373ed01e5cea396d8dc5d59b
Kamil Walkowiak <kamil.walek21@gmail.com>
2015-07-06 19:36:05 +0000

    Working clinics assignments

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,5 +1,6 @@
 class DoctorsController < ApplicationController
-  before_action :set_doctor, only: [:show, :edit, :update, :destroy]
+  before_action :set_doctor, only: [:show, :edit, :update, :destroy,
+    :assign_clinic, :unassign_clinic]
 
   respond_to :html
 
@@ -9,7 +10,12 @@ class DoctorsController < ApplicationController
   end
 
   def show
-    respond_with(@doctor)
+    if current_admin
+      @clinics = Clinic.where.not(id: @doctor.clinic_ids)
+      respond_with(@doctor)
+    else
+      redirect_to root_url, notice: "You are not allowed to do that!"
+    end
   end
 
   def new
@@ -32,7 +38,24 @@ class DoctorsController < ApplicationController
   end
 
   def destroy
-    @doctor.destroy
+    if !current_patient
+      @doctor.destroy
+      respond_with(@doctor)
+    else
+      redirect_to root_url, notice: "You are not allowed to delete doctors!"
+    end
+  end
+  
+  def assign_clinic
+    clinic = Clinic.find(params[:doctor][:clinics])
+    @doctor.clinics << clinic
+    @doctor.save
+    respond_with(@doctor)
+  end
+  
+  def unassign_clinic
+    @doctor.clinics.delete(params[:doctor][:clinics])
+    @doctor.save
     respond_with(@doctor)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   resources :patients
   
   post 'patients/activate/:id' => 'patients#activate', as: :activate_patient
+  patch 'doctors/:id/assign' => 'doctors#assign_clinic', as: :doctor_assign_clinic
+  patch 'doctors/:id/unassign' => 'doctors#unassign_clinic', as: :doctor_unassign_clinic
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
